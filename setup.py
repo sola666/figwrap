e1da627e6480d4b844f050dc57bdb417efbcb3dc
S O L A <58261219+sola666@users.noreply.github.com>
2022-12-12 06:22:36 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='figwrap',
+    version='1.0.0',
+    packages=['figwrap'],
+    url='https://github.com/sola666/figwrap',
+    license='MIT',
+    author='sola187',
+    author_email='sola@sola666.xyz',
+    description='A simple class to wrap pyfiglet and colorama to make it easier to render figlet text.'
+)
